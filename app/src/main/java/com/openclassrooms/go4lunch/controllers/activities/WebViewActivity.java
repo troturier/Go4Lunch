07b717e81160885ccb7a0a325904862509655e9f
troturier <thibaultroturier@gmail.com>
2018-08-17 15:57:39 +0200

    Mise à jour de la WebViewActivity selon les recommandations d'IntelliJ

--- app/src/main/java/com/openclassrooms/go4lunch/controllers/activities/WebViewActivity.java
+++ app/src/main/java/com/openclassrooms/go4lunch/controllers/activities/WebViewActivity.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.go4lunch.controllers.activities;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import com.openclassrooms.go4lunch.R;
  */
 public class WebViewActivity extends Activity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
